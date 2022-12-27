b85956541549acb01ba239edcdd689b4aa346f98
jagm03 <jonathan.gonzalez@kaust.edu.sa>
2022-12-27 13:16:44 +0300

    auxiliary

--- R/multifiksel.R
+++ R/multifiksel.R
@@ -337,12 +337,3 @@ MultiFiksel <- local({
 })
 
 
-intermaker <- function(f, blank) {
-  # f is the creator function like 'Fiksel'
-  class(f) <- c("intermaker", class(f))
-  # blank is the prototype interaction object: extract some fields
-  desired <- c("creator", "name", "par", "parnames", "pardesc")
-  avail <- desired[desired %in% names(blank)]
-  attr(f, "b") <- blank[avail]
-  return(f)
-}
