7707f06aff6377486c931c5eb013fb8417fe8eb1
jagm03 <jonathan.gonzalez@kaust.edu.sa>
2023-02-02 18:36:05 +0300

    Experiments2

--- R/DataReading.R
+++ R/DataReading.R
@@ -1,10 +1,11 @@
 #Preparing data
+library(spatstat.utils)
 PP <- rthin(unmark(Cells$pp$"13") %mark% Cells$pp$'13'$marks$Immune, .1)
 plot(PP)
 
 Gamma <- matrix(rep(2, 25), 5, 5)
 Radii <- matrix(rep(12, 25), 5, 5)
 
-A <- ppm(PP ~ 1, MultiFiksel(iradii = Radii, igammaii = Gamma))
+A <- ppm(PP ~ x + y, MultiFiksel(iradii = Radii, igammaii = Gamma))
 plot(fitin(A))
-B <- rmhmodel() 
+plot(A)
